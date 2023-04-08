244ea4b7ef93448db4eaa0e13fb5827111a80cfe
EC2 Default User <ec2-user@ip-172-31-27-249.ap-northeast-1.compute.internal>
2023-04-08 07:53:55 +0000

    modelの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :spot_genres
+  devise_for :favorites
+  devise_for :post_comments
+  devise_for :posts
+  devise_for :admins
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
