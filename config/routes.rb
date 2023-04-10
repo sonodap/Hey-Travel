8a61b57b087bff9af7871e195722b4c86ceb1b45
EC2 Default User <ec2-user@ip-172-31-27-249.ap-northeast-1.compute.internal>
2023-04-10 11:30:33 +0000

    bootstrapとfontawesomeのインストール完了、ルーティング作成完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,40 +1,40 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'spot_genres/index'
-    get 'spot_genres/edit'
-  end
-  namespace :admin do
-    get 'users/show'
-    get 'users/index'
-    get 'users/unsubscribe'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :public do
-    get 'users/show'
-    get 'users/edit'
-    get 'users/unsubscribe'
-  end
-  namespace :public do
-    get 'posts/new'
-    get 'posts/create'
-    get 'posts/imdex'
-    get 'posts/show'
-    get 'posts/destroy'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
+  get 'homes/top'
+  get 'homes/about'
+  root :to =>"homes#top"
+  get "home/about"=>"homes#about"
+  get "search" => "searches#search"
+
   devise_for :users,skip: [:passwords], controllers:{
      registrations: "public/registrations",
      sessions: 'public/sessions'
   }
 
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
-  sessions: "admin/sessions"
-}
+    sessions: "admin/sessions"
+  }
+
+  namespace :admin do
+    get 'homes/top'
+    resources :spot_genres, only:[:index, :edit, :create, :update]
+    resources :users, only: [:index, :show,]
+    get 'users/unsubscribe' => 'users#unsubscribe', as: 'admin_unsubscribe'
+    patch 'users/withdraw' => 'users#withdraw', as: 'admin_withdraw'
+    delete 'admin/post_comment/destroy' => 'admin/post_comments#destroy', as: 'admin_destroy_comment'
+  end
+
+  scope module: :public do
+    post 'users/my_page' => 'users#show', as: 'user'
+    get 'users/information/edit' => 'users#edit', as: 'edit_user'
+    patch 'users/information' => 'users#update', as: 'update_user'
+    get 'users/unsubscribe' => 'users#unsubscribe', as: 'unsubscribe'
+    patch 'users/withdraw' => 'users#withdraw', as: 'withdraw'
+
+    resources :posts, only: [:index,:create,:destroy,:update]
+    post 'posts/my_post' => 'posts#show'
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
