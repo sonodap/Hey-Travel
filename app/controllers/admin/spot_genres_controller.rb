49c2912648115346a7536295535074600f58cc78
EC2 Default User <ec2-user@ip-172-31-27-249.ap-northeast-1.compute.internal>
2023-04-16 06:35:10 +0000

    管理者側のジャンル機能実装完了

--- app/controllers/admin/spot_genres_controller.rb
+++ app/controllers/admin/spot_genres_controller.rb
@@ -1,13 +1,28 @@
 class Admin::SpotGenresController < ApplicationController
   def index
+    @spot_genre = Spot_genre.new
+    @spot_genres = Spot_genre.all
   end
 
   def create
+    @spot_genre = Spot_genre.new(spot_genre_params)
+    @spot_genre = save
+    redirect_to admin_spot_genres_path
   end
 
   def edit
+    @spot_genre = Spot_genre.find(params[:id])
   end
 
   def update
+    @spot_genre = Spot_genre.fine(params[:id])
+    @spot_genre.update(spot_genre_params)
+    redirect_to admin_spot_genres_path
+  end
+
+  private
+
+  def spot_genre_params
+    params.require(:spot_genres).permit(:spot_genre)
   end
 end
