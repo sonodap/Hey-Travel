99bb6f3b79d1fab06574b2456b630f2f9ba65e99
EC2 Default User <ec2-user@ip-172-31-27-249.ap-northeast-1.compute.internal>
2023-04-17 17:58:28 +0000

    ユーザー編集画面のレイアウト調整

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,7 +1,7 @@
 class Public::UsersController < ApplicationController
 
   before_action :authenticate_user!
-  before_action :ensure_guest_user, only: [:edit]
+  # before_action :ensure_guest_user, only: [:edit]
   # before_action :ensure_correct_user, only: [:edit,:update]
 
   def mypage
@@ -63,4 +63,11 @@ class Public::UsersController < ApplicationController
       redirect_to user_path(current_user)
     end
   end
+  
+  # def ensure_guest_user
+  #   @user = User.find(params[:id])
+  #   if @user.name == "guestuser"
+  #     redirect_to user_path(current_user) , notice: 'ゲストユーザーはプロフィール編集画面へ遷移できません。'
+  #   end
+  # end  
 end
